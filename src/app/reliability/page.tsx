de4b99d6a3ad20481ca2f033a8313a902d197b95
Muhammad Abubakar Jamiu <muhammadabubakarjamiu@gmail.com>
2024-11-01 18:05:46 +0100

    first commit

---
+++ src/app/reliability/page.tsx
@@ -0,0 +1,9 @@
+import reliabilityImg from '/public/reliability.jpg';
+import Hero from '@/components/hero';
+export default function ReliabilityPage() {
+  return (
+   <Hero
+   imgData={reliabilityImg} imgAlt='reliability image' title="We Are Highly Reliability"/>
+  );
+}
+ 
